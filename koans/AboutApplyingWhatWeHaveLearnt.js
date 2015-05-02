fe42dad484e679ff251f87edea3328c3055add47
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-05-02 12:28:18 -0700

    First two tests in ApplyingWhatWeHaveLearnt

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,17 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var isMushrooms = function(ingredient) {
+          return ingredient === "mushrooms";
+      };
+      
+      var nutFree = _(products).filter(function(food) {return food.containsNuts === false});
+      productsICanEat = _(nutFree).filter(function(food) {
+          return !_(food.ingredients).any(isMushrooms);
+      });
+      
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
